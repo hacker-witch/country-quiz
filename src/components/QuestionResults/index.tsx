7660554ab65cadd32af810a9aa247d6af16ce5cd
Mitzi <mitzisantos13@gmail.com>
2021-08-12 17:13:27 -0300

    Add a button to QuestionResults

--- src/components/QuestionResults/index.tsx
+++ src/components/QuestionResults/index.tsx
@@ -1,5 +1,6 @@
 import { QuizBox } from "../QuizBox";
 import { Question } from "../Question";
+import { Button } from "../Button";
 
 interface QuestionResultsProps {
   question: string;
@@ -8,5 +9,6 @@ interface QuestionResultsProps {
 export const QuestionResults = ({ question }: QuestionResultsProps) => (
   <QuizBox>
     <Question>{question}</Question>
+    <Button type="button">Next</Button>
   </QuizBox>
 );
