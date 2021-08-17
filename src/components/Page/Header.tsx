ae8836741496257aeaa757f38bc8e54df52d2dee
Mitzi <mitzisantos13@gmail.com>
2021-08-17 17:50:50 -0300

    Adjust styles of Header ro keep the heading above the box and algined to the left in bigger screens when there is no illustration

--- src/components/Page/Header.tsx
+++ src/components/Page/Header.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { ReactComponent as Illustration } from "img/header-illustration.svg";
 
 interface HeaderProps {
@@ -6,25 +6,50 @@ interface HeaderProps {
 }
 
 export const Header = ({ withIllustration }: HeaderProps) => (
-  <Wrapper>
-    <Heading>COUNTRY QUIZ</Heading>
+  <Wrapper withIllustration={withIllustration}>
+    <Heading withIllustration={withIllustration}>COUNTRY QUIZ</Heading>
     {withIllustration ? <StyledIllustration /> : null}
   </Wrapper>
 );
 
-const Wrapper = styled.header`
+const withIllustrationStyles = {
+  wrapper: css`
+    @media (min-width: 31rem) {
+      position: relative;
+      top: 42px;
+      padding: 0;
+    }
+  `,
+
+  heading: css`
+    @media (min-width: 31rem) {
+      position: relative;
+      top: 0.625rem;
+    }
+  `,
+};
+
+interface WrapperProps {
+  withIllustration: boolean;
+}
+
+const Wrapper = styled.header<WrapperProps>`
   display: flex;
   align-items: start;
   padding: 2rem 0;
 
   @media (min-width: 31rem) {
-    position: relative;
-    top: 42px;
     padding: 0;
   }
+
+  ${(props) => (props.withIllustration ? withIllustrationStyles.wrapper : null)}
 `;
 
-const Heading = styled.h1`
+interface HeadingProps {
+  withIllustration: boolean;
+}
+
+const Heading = styled.h1<HeadingProps>`
   flex: 1;
   text-align: center;
   font-size: 2.25rem;
@@ -33,9 +58,9 @@ const Heading = styled.h1`
 
   @media (min-width: 31rem) {
     text-align: left;
-    position: relative;
-    top: 0.625rem;
   }
+
+  ${(props) => (props.withIllustration ? withIllustrationStyles.heading : null)}
 `;
 
 const StyledIllustration = styled(Illustration)`
