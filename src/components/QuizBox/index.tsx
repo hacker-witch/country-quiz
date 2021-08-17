27ae45db7a9500ce3771de5e84aae5ff14ee7c8c
Mitzi <mitzisantos13@gmail.com>
2021-08-17 18:09:22 -0300

    Make all paddings in QuizBox equal to 2rem

--- src/components/QuizBox/index.tsx
+++ src/components/QuizBox/index.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const QuizBox = styled.div`
-  padding: 4.25rem 2rem 2rem 2rem;
+  padding: 2rem;
   background: #fff;
   border-radius: 1.5rem;
 `;
