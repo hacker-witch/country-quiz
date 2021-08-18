c83f747c383ccf3097abe4f4d3dec6f4948faaa5
Mitzi <mitzisantos13@gmail.com>
2021-08-18 12:21:13 -0300

    Add the QuizResults heading

--- src/components/QuizResults/index.tsx
+++ src/components/QuizResults/index.tsx
@@ -5,10 +5,19 @@ import { ReactComponent as Illustration } from "img/results-illustration.svg";
 export const QuizResults = () => (
   <Page withHeaderIllustration={false}>
     <StyledIllustration />
+    <Heading>Results</Heading>
   </Page>
 );
 
 const StyledIllustration = styled(Illustration)`
   display: block;
   margin: 1.625rem auto 0 auto;
+`;
+
+const Heading = styled.h2`
+  margin-top: 4.5rem;
+  text-align: center;
+  font-size: 3rem;
+  font-weight: 700;
+  color: #1d355d;
 `;
