3e9d0719f268002df913b249eac35ec023acbddc
Mitzi <mitzisantos13@gmail.com>
2021-08-18 12:16:58 -0300

    Replace placeholder text of QuizResults with an illustration

--- src/components/QuizResults/index.tsx
+++ src/components/QuizResults/index.tsx
@@ -1,5 +1,14 @@
 import { Page } from "../Page";
+import styled from "styled-components";
+import { ReactComponent as Illustration } from "img/results-illustration.svg";
 
 export const QuizResults = () => (
-  <Page withHeaderIllustration={false}>placeholder</Page>
+  <Page withHeaderIllustration={false}>
+    <StyledIllustration />
+  </Page>
 );
+
+const StyledIllustration = styled(Illustration)`
+  display: block;
+  margin: 1.625rem auto 0 auto;
+`;
