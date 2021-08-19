647acf22ea6f7eac5a3243f6d25dbbb6a740a11e
Mitzi <mitzisantos13@gmail.com>
2021-08-19 13:40:10 -0300

    Remove placeholder data and get the data from the countries api instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Quiz, QuestionResults, QuizResults } from "components";
-
-const data = {
-  question: "Kuala Lumpur is the capital of",
-  answerOptions: ["vietnam", "malaysia", "sweden", "austria"],
-  correctAnswer: "malaysia",
-};
+import { chooseIndex } from "utils";
 
 enum QuizStatus {
   Answering = "ANSWERING",
@@ -14,9 +9,33 @@ enum QuizStatus {
 }
 
 export const App = () => {
+  const [currentQuestion, setCurrentQuestion] = useState("");
+  const [answerOptions, setAnswerOptions] = useState<string[]>([]);
+  const [correctAnswer, setCorrectAnswer] = useState<string | null>(null);
   const [chosenAnswer, setChosenAnswer] = useState<string | null>(null);
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [quizStatus, setQuizStatus] = useState(QuizStatus.Answering);
+
+  useEffect(() => {
+    const baseURL = "https://restcountries.eu/rest/v2";
+    fetch(`${baseURL}/all/?fields=name;capital`)
+      .then((response) => response.json())
+      .then((data) => {
+        const randomCountries = [
+          data[chooseIndex(data)],
+          data[chooseIndex(data)],
+          data[chooseIndex(data)],
+          data[chooseIndex(data)],
+        ];
+
+        const countryNames = randomCountries.map((country) => country.name);
+        setAnswerOptions(countryNames);
+
+        const correctCountry = randomCountries[chooseIndex(randomCountries)];
+        setCurrentQuestion(`${correctCountry.capital} is the capital of`);
+        setCorrectAnswer(correctCountry.name);
+      });
+  }, []);
 
   const finishQuiz = () => setQuizStatus(QuizStatus.GameOver);
 
@@ -32,7 +51,7 @@ export const App = () => {
     setChosenAnswer(answer);
     setQuizStatus(QuizStatus.ViewingQuestionResults);
 
-    if (answer === data.correctAnswer) {
+    if (answer === correctAnswer) {
       setCorrectAnswers(correctAnswers + 1);
     }
   };
@@ -41,8 +60,8 @@ export const App = () => {
     case QuizStatus.Answering:
       return (
         <Quiz
-          question={data.question}
-          answerOptions={data.answerOptions}
+          question={currentQuestion}
+          answerOptions={answerOptions}
           onSubmit={answerQuestion}
         />
       );
@@ -50,10 +69,10 @@ export const App = () => {
     case QuizStatus.ViewingQuestionResults:
       return (
         <QuestionResults
-          question={data.question}
-          answerOptions={data.answerOptions}
+          question={currentQuestion}
+          answerOptions={answerOptions}
           chosenAnswer={chosenAnswer!}
-          correctAnswer={data.correctAnswer}
+          correctAnswer={correctAnswer!}
           finishQuiz={finishQuiz}
           continueQuiz={continueQuiz}
         />
