88c8b1f49f45cd12750565337eb51e7e3187bae9
Mitzi <mitzisantos13@gmail.com>
2021-08-20 19:14:19 -0300

    Replace 'any' with a generic in chooseUniqueItems

--- src/utils/chooseUniqueItems.ts
+++ src/utils/chooseUniqueItems.ts
@@ -1,7 +1,7 @@
 import { chooseIndex } from "./chooseIndex";
 
-export const chooseUniqueItems = (array: any[], quantity: number) => {
-  const chosenItems: any[] = [];
+export const chooseUniqueItems = <T>(array: T[], quantity: number) => {
+  const chosenItems: T[] = [];
 
   while (chosenItems.length < quantity) {
     const index = chooseIndex(array);
