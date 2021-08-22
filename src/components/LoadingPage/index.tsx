adb7015c187cb88fc60b41c86676392a1b2a686f
Mitzi <mitzisantos13@gmail.com>
2021-08-22 12:24:00 -0300

    Add a spinner to the loading page

--- src/components/LoadingPage/index.tsx
+++ src/components/LoadingPage/index.tsx
@@ -1,3 +1,43 @@
+import styled, { keyframes } from "styled-components";
 import { Page } from "../Page";
 
-export const LoadingPage = () => <Page>Loading...</Page>;
+export const LoadingPage = () => (
+  <Page>
+    <Container>
+      <Spinner aria-label="Loading..." />
+    </Container>
+  </Page>
+);
+
+const Container = styled.div`
+  height: 449px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Spinner = styled.div`
+  width: 9.375rem;
+  height: 9.375rem;
+
+  ::before {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    border: 0.5rem solid transparent;
+    border-top-color: rgba(96, 102, 208, 0.8);
+    animation: ${spin} 1s infinite linear;
+  }
+`;
