12db9516930a6e5921b1bb19828c311e1e6dd0d3
Mitzi <mitzisantos13@gmail.com>
2021-08-23 14:57:52 -0300

    Update chooseUniqueItems to call chooseItem instead of chooseIndex

--- src/utils/chooseUniqueItems.ts
+++ src/utils/chooseUniqueItems.ts
@@ -1,11 +1,10 @@
-import { chooseIndex } from "./chooseIndex";
+import { chooseItem } from "./chooseItem";
 
 export const chooseUniqueItems = <T>(array: T[], quantity: number) => {
   const chosenItems: T[] = [];
 
   while (chosenItems.length < quantity) {
-    const index = chooseIndex(array);
-    const item = array[index];
+    const item = chooseItem(array);
     if (!chosenItems.includes(item)) {
       chosenItems.push(item);
     }
