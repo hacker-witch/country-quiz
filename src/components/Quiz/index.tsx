1c617b2346901a44702823c161428e8f48a283f6
Mitzi <mitzisantos13@gmail.com>
2021-08-23 18:54:41 -0300

    Move QuizForm out of Quiz

--- src/components/Quiz/index.tsx
+++ src/components/Quiz/index.tsx
@@ -1,16 +1,10 @@
-import { useState, FormEvent } from "react";
-import styled from "styled-components";
+import { useState } from "react";
 import { Page } from "../Page";
-import { Question } from "../Question";
-import { AnswerOptionsGroup } from "../AnswerOptionsGroup";
-import { Button } from "../Button";
-import { AnswerOptionField } from "./AnswerOptionField";
+import { QuizForm } from "../QuizForm";
 import { QuestionResults } from "../QuestionResults";
 import { QuizResults } from "../QuizResults";
 import { Country } from "data";
 import { generateQuestionFromCountryList } from "quiz";
-
-const letters = ["a", "b", "c", "d"];
 
 enum QuizStatus {
   Answering = "ANSWERING",
@@ -96,65 +90,3 @@ export const Quiz = ({ countries }: QuizProps) => {
       return null;
   }
 };
-
-interface QuizFormProps {
-  flag?: string;
-  question: string;
-  answerOptions: string[];
-  chosenAnswer: string;
-  answerQuestion: (answer: string) => void;
-}
-
-const QuizForm = ({
-  flag,
-  question,
-  answerOptions,
-  chosenAnswer,
-  answerQuestion,
-}: QuizFormProps) => {
-  const [checkedAnswer, setCheckedAnswer] = useState(chosenAnswer);
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    answerQuestion(checkedAnswer);
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      {flag ? <Flag src={flag} alt="" /> : null}
-      <Fieldset>
-        <Question as="legend">{question}</Question>
-        <AnswerOptionsGroup>
-          {answerOptions.map((option, index) => (
-            <AnswerOptionField
-              key={index}
-              letter={letters[index]}
-              value={option}
-              isChecked={option === checkedAnswer}
-              onChange={(_) => setCheckedAnswer(option)}
-            />
-          ))}
-        </AnswerOptionsGroup>
-      </Fieldset>
-
-      <SubmitAnswerButton type="submit">Submit</SubmitAnswerButton>
-    </form>
-  );
-};
-
-const Fieldset = styled.fieldset`
-  border: none;
-`;
-
-const Flag = styled.img`
-  margin-bottom: 1.75rem;
-  width: 5.25rem;
-  height: 3.375rem;
-  border-radius: 0.25rem;
-  box-shadow: 0 0.25rem 1.5rem 0 #0000001a;
-`;
-
-const SubmitAnswerButton = styled(Button)`
-  margin-top: 1.5rem;
-  margin-left: auto;
-`;
---
+++ src/components/QuizForm/index.tsx
@@ -0,0 +1,70 @@
+import { useState, FormEvent } from "react";
+import styled from "styled-components";
+import { Question } from "../Question";
+import { AnswerOptionsGroup } from "../AnswerOptionsGroup";
+import { Button } from "../Button";
+import { AnswerOptionField } from "../Quiz/AnswerOptionField";
+
+const letters = ["a", "b", "c", "d"];
+
+interface QuizFormProps {
+  flag?: string;
+  question: string;
+  answerOptions: string[];
+  chosenAnswer: string;
+  answerQuestion: (answer: string) => void;
+}
+
+export const QuizForm = ({
+  flag,
+  question,
+  answerOptions,
+  chosenAnswer,
+  answerQuestion,
+}: QuizFormProps) => {
+  const [checkedAnswer, setCheckedAnswer] = useState(chosenAnswer);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    answerQuestion(checkedAnswer);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {flag ? <Flag src={flag} alt="" /> : null}
+      <Fieldset>
+        <Question as="legend">{question}</Question>
+        <AnswerOptionsGroup>
+          {answerOptions.map((option, index) => (
+            <AnswerOptionField
+              key={index}
+              letter={letters[index]}
+              value={option}
+              isChecked={option === checkedAnswer}
+              onChange={(_) => setCheckedAnswer(option)}
+            />
+          ))}
+        </AnswerOptionsGroup>
+      </Fieldset>
+
+      <SubmitAnswerButton type="submit">Submit</SubmitAnswerButton>
+    </form>
+  );
+};
+
+const Fieldset = styled.fieldset`
+  border: none;
+`;
+
+const Flag = styled.img`
+  margin-bottom: 1.75rem;
+  width: 5.25rem;
+  height: 3.375rem;
+  border-radius: 0.25rem;
+  box-shadow: 0 0.25rem 1.5rem 0 #0000001a;
+`;
+
+const SubmitAnswerButton = styled(Button)`
+  margin-top: 1.5rem;
+  margin-left: auto;
+`;
