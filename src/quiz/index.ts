4666e4b5167537a7b0ebaf70735f4966ab1895f1
Mitzi <mitzisantos13@gmail.com>
2021-08-23 19:23:49 -0300

    Delete generateQuestion

--- src/quiz/index.ts
+++ src/quiz/index.ts
@@ -1,6 +1,4 @@
-import { fetchAllCountries } from "data";
 import { chooseUniqueItems, chooseIndex, chooseItem } from "utils";
-import { ApplicationError, NetworkError } from "errors";
 
 interface Country {
   name: string;
@@ -26,37 +24,6 @@ export interface Question {
   answerOptions: string[];
   correctAnswer: string;
 }
-
-export const generateQuestion = async (): Promise<Question> => {
-  try {
-    const questionType = chooseItem(allQuestionTypes);
-    const countries =
-      questionType === QuestionType.Capital
-        ? await fetchAllCountries({ fields: ["name", "capital"] })
-        : await fetchAllCountries({ fields: ["name", "flag"] });
-    const randomCountries = chooseCountries(countries);
-    const countryNames = randomCountries.map((country) => country.name);
-    const correctCountry = randomCountries[chooseIndex(randomCountries)];
-
-    const question = {
-      flag:
-        questionType === QuestionType.Capital ? undefined : correctCountry.flag,
-      answerOptions: countryNames,
-      title:
-        questionType === QuestionType.Capital
-          ? `${correctCountry.capital} is the capital of`
-          : "Which country does this flag belong to?",
-      correctAnswer: correctCountry.name,
-    };
-    return question;
-  } catch (error) {
-    if (error instanceof ApplicationError) {
-      throw error;
-    }
-
-    throw new NetworkError();
-  }
-};
 
 export const generateQuestionFromCountryList = (
   countries: Required<Country>[]
