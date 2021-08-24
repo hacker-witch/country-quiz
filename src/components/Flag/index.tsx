9acf18a342543af1688f89aaaab0dc38cbec275c
Mitzi <mitzisantos13@gmail.com>
2021-08-24 17:52:55 -0300

    Make box shadow of the flag more proeminent so that white flags are more visible

--- src/components/Flag/index.tsx
+++ src/components/Flag/index.tsx
@@ -5,5 +5,6 @@ export const Flag = styled.img`
   width: 5.25rem;
   height: 3.375rem;
   border-radius: 0.25rem;
-  box-shadow: 0 0.25rem 1.5rem 0 #0000001a;
+  box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgb(0, 0, 0, 0.12),
+    0 2px 4px -1px rgb(0, 0, 0, 0.3); ;
 `;
