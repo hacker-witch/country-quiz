5ce5f58c94698ff7022a751334366eeea4ef25e8
Mitzi <mitzisantos13@gmail.com>
2021-08-24 17:15:20 -0300

    Keep track of past question titles in useQuiz

--- src/hooks/useQuiz/index.ts
+++ src/hooks/useQuiz/index.ts
@@ -16,11 +16,13 @@ export const useQuiz = (countries: Country[]) => {
   const [chosenAnswer, setChosenAnswer] = useState(
     initialCurrentQuestion.answerOptions[0]
   );
+  const [pastQuestionTitles, setPastQuestionTitles] = useState<string[]>([]);
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [quizStatus, setQuizStatus] = useState(QuizStatus.Answering);
 
   const loadNextQuestion = () => {
     const question = generateQuestionFromCountryList(countries);
+    setPastQuestionTitles([...pastQuestionTitles, question.title]);
     setCurrentQuestion(question);
     setChosenAnswer(question.answerOptions[0]);
   };
